92264fdeef9902071cc5e0a3157c3ec1616ba500
olavopoletti <olavo.poletti@outlook.com>
2023-02-05 07:58:04 -0300

    app server

--- app.py
+++ app.py
@@ -13,10 +13,27 @@ server = app.server
 app.layout = html.Div(
         children=[
         html.Img(src=app.get_asset_url('db.jpg'))
-        ]              
-)
+        ],
+        style={
+        'verticalAlign':'middle',
+        'textAlign': 'center',
+        'width':'100%',
+        'height':'100%',
+        'top':'0px',
+        'left':'0px',
+        'z-index':'1000',
+        'background-image': 'url(assets/db.jpg)',
+        'position':'fixed',
+        'background-size': 'contain',
+        'background-size': 'cover',
+        'background-size': '100%',
+        'background-color': 'rgba(0, 0, 0, 0.55)',
+        'background-blend-mode': 'darken',
+        },                   
+)              
+
 if __name__ == "__main__":
     from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    serve(app)
 
 
